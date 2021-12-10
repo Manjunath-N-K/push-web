6f9bac0f68040ce40aab7c0bcaa5e67eb5869107
N <237604@NTTDATA.COM>
2021-12-10 15:24:54 +0530

    'fix'

--- push.js
+++ push.js
@@ -8,5 +8,7 @@ let vapidKeys=
 
 push.setVapidDetails('mailto:test@code.co.uk',vapidKeys.publicKey,vapidKeys.privateKey)
 
-let sub={};
+let sub={
+    "endpoint":"https://fcm.googleapis.com/fcm/send/eatS59il9Zk:APA91bFCx95Kx7K3Raf8UuypulAeVruAYl5gh4PWqs8YhPOya0nCp_V5hrhY_f3mZ9Y7NEzuzv7fhiVFbxgRw0rLcxh5VD6PrSfMoJfjBS6MWbDqXDEkfSs0wOlfabkOpGJzaqDEgdPN","expirationTime":null,"keys":{"p256dh":"BCutJ1T4hyF0a2zcS6PgcMZ80REmdGsOHxVgUJYcfgv17RQKRnD57-0HRY86fYIWEwm9Jr5l2R22u1lW1FDvGBU","auth":"oGDcw5a-csEYDNKef_yALw"}
+};
 push.sendNotification(sub,'test message')
